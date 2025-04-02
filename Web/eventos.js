6d494381e830d23690e7f0b1918d3070b08dda43
Thais Nuñez <thaisagullo@gmail.com>
2025-04-02 21:37:38 +0200

    He cambado la extension para que se pueda ver sin un servidor local

---
+++ Web/eventos.js
@@ -0,0 +1,207 @@
+
+const event = 
+     {
+    "eventos": [
+      {
+        "id_evento": "E001",
+        "artista": "Melendi",
+        "fecha": "2024-05-10",
+        "ubicacion": "Madrid",
+        "precio": 50.00,
+        "disponible": 1,
+        "tipo": "Concierto",
+        "imagen": "../Imagenes/melendi.jpg"
+      },
+      {
+        "id_evento": "E002",
+        "artista": "Shakira",
+        "fecha": "2024-06-15",
+        "ubicacion": "Barcelona",
+        "precio": 70.00,
+        "disponible": 1,
+        "tipo": "Concierto",
+        "imagen": "../Imagenes/shakira.jpg"
+      },
+      {
+        "id_evento": "E003",
+        "artista": "Tributo Queen",
+        "fecha": "2024-07-20",
+        "ubicacion": "Valencia",
+        "precio": 30.00,
+        "disponible": 0,
+        "tipo": "Concierto",
+        "imagen": "../Imagenes/tributoqueen.jpg"
+      },
+      {
+        "id_evento": "E004",
+        "artista": "Metallica",
+        "fecha": "2024-08-05",
+        "ubicacion": "Sevilla",
+        "precio": 90.00,
+        "disponible": 1,
+        "tipo": "Concierto",
+        "imagen": "../Imagenes/metallica.jpg"
+      },
+      {
+        "id_evento": "E005",
+        "artista": "Coldplay",
+        "fecha": "2024-09-12",
+        "ubicacion": "Bilbao",
+        "precio": 80.00,
+        "disponible": 1,
+        "tipo": "Concierto",
+        "imagen": "../Imagenes/coldplay.jpg"
+      },
+      {
+        "id_evento": "E006",
+        "artista": "Hamlet",
+        "fecha": "2024-05-18",
+        "ubicacion": "Madrid",
+        "precio": 40.00,
+        "disponible": 0,
+        "tipo": "Teatro",
+        "imagen": "../Imagenes/hamlet.jpg"
+      },
+      {
+        "id_evento": "E007",
+        "artista": "El Rey León",
+        "fecha": "2024-06-25",
+        "ubicacion": "Barcelona",
+        "precio": 60.00,
+        "disponible": 0,
+        "tipo": "Teatro",
+        "imagen": "../Imagenes/elreyleon.jpg"
+      },
+      {
+        "id_evento": "E008",
+        "artista": "Bernarda",
+        "fecha": "2024-07-15",
+        "ubicacion": "Sevilla",
+        "precio": 35.00,
+        "disponible": 0,
+        "tipo": "Teatro",
+        "imagen": "../Imagenes/bernarda.jpg"
+      },
+      {
+        "id_evento": "E009",
+        "artista": "Don Juan Tenorio",
+        "fecha": "2024-08-10",
+        "ubicacion": "Valencia",
+        "precio": 45.00,
+        "disponible": 1,
+        "tipo": "Teatro",
+        "imagen": "../Imagenes/donjuantenorio.jpg"
+      },
+      {
+        "id_evento": "E010",
+        "artista": "Romeo y Julieta",
+        "fecha": "2024-09-20",
+        "ubicacion": "Granada",
+        "precio": 50.00,
+        "disponible": 0,
+        "tipo": "Teatro",
+        "imagen": "../Imagenes/romeoyjulieta.jpg"
+      },
+      {
+        "id_evento": "E011",
+        "artista": "Comic Con",
+        "fecha": "2024-06-10",
+        "ubicacion": "Madrid",
+        "precio": 25.00,
+        "disponible": 0,
+        "tipo": "Evento",
+        "imagen": "../Imagenes/comiccon.jpg"
+      },
+      {
+        "id_evento": "E012",
+        "artista": "Feria del Libro",
+        "fecha": "2024-07-05",
+        "ubicacion": "Barcelona",
+        "precio": 15.00,
+        "disponible": 0,
+        "tipo": "Evento",
+        "imagen": "../Imagenes/feriadellibro.jpg"
+      },
+      {
+        "id_evento": "E013",
+        "artista": "Gaming Expo",
+        "fecha": "2024-08-12",
+        "ubicacion": "Sevilla",
+        "precio": 35.00,
+        "disponible": 1,
+        "tipo": "Evento",
+        "imagen": "../Imagenes/gamingexpo.jpg"
+      },
+      {
+        "id_evento": "E014",
+        "artista": "Feria de la Ciencia",
+        "fecha": "2024-09-01",
+        "ubicacion": "Valencia",
+        "precio": 20.00,
+        "disponible": 0,
+        "tipo": "Evento",
+        "imagen": "../Imagenes/feriadelaciencia.jpg"
+      },
+      {
+        "id_evento": "E015",
+        "artista": "Auto Show",
+        "fecha": "2024-10-15",
+        "ubicacion": "Bilbao",
+        "precio": 40.00,
+        "disponible": 1,
+        "tipo": "Evento",
+        "imagen": "../Imagenes/autoshow.jpg"
+      },
+      {
+        "id_evento": "E016",
+        "artista": "Tomorrowland",
+        "fecha": "2024-07-20",
+        "ubicacion": "Bélgica",
+        "precio": 200.00,
+        "disponible": 0,
+        "tipo": "Festival",
+        "imagen": "../Imagenes/tomorrowland.jpg"
+      },
+      {
+        "id_evento": "E017",
+        "artista": "Coachella",
+        "fecha": "2024-04-15",
+        "ubicacion": "California",
+        "precio": 300.00,
+        "disponible": 1,
+        "tipo": "Festival",
+        "imagen": "../Imagenes/coachella.jpg"
+      },
+      {
+        "id_evento": "E018",
+        "artista": "Glastongury",
+        "fecha": "2024-06-25",
+        "ubicacion": "Reino Unido",
+        "precio": 250.00,
+        "disponible": 0,
+        "tipo": "Festival",
+        "imagen": "../Imagenes/glastongury.jpg"
+      },
+      {
+        "id_evento": "E019",
+        "artista": "Rock in Rio",
+        "fecha": "2024-09-10",
+        "ubicacion": "Brasil",
+        "precio": 220.00,
+        "disponible": 0,
+        "tipo": "Festival",
+        "imagen": "../Imagenes/rockinrio.jpg"
+      },
+      {
+        "id_evento": "E020",
+        "artista": "Lollapalooza",
+        "fecha": "2024-08-05",
+        "ubicacion": "Argentina",
+        "precio": 180.00,
+        "disponible": 1,
+        "tipo": "Festival",
+        "imagen": "../Imagenes/lollapalooza.jpg"
+      }
+    ]
+  }
+  
